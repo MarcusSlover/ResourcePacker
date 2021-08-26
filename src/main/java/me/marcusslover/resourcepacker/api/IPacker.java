94e0786f48ba51f9e4926c8c26d0c27c0c3acf3d
MarcusSlover <mariuszek016@gmail.com>
2021-08-26 20:57:51 +0200

    * fix

--- src/main/java/me/marcusslover/resourcepacker/api/IPacker.java
+++ src/main/java/me/marcusslover/resourcepacker/api/IPacker.java
@@ -4,6 +4,7 @@ import me.marcusslover.resourcepacker.core.internal.RPBlockRegistry;
 import me.marcusslover.resourcepacker.core.internal.RPItemRegistry;
 import me.marcusslover.resourcepacker.core.resource.ResourceHelper;
 
+import java.io.File;
 import java.util.List;
 
 public interface IPacker {
@@ -11,12 +12,19 @@ public interface IPacker {
 
     RPItemRegistry items();
 
+    void setLogo(String path);
+    String logo();
+
     void setName(String name);
+    String name();
 
     void setPrefix(String prefix);
+    String prefix();
 
     void setDescription(List<String> description);
+    List<String> description();
 
     ResourceHelper getResources();
+    File getOutput();
 
 }
