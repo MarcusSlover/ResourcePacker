4250990169caccf0bb07fe0914d3499bcf672889
MarcusSlover <mariuszek016@gmail.com>
2022-05-15 17:42:05 +0200

    * package update

---
+++ src/main/java/com/marcusslover/resourcepacker/core/generator/BlockGenerator.java
@@ -0,0 +1,143 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 MarcusSlover
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package com.marcusslover.resourcepacker.core.generator;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.marcusslover.resourcepacker.util.JsonUtil;
+import com.marcusslover.resourcepacker.api.IGenerator;
+import com.marcusslover.resourcepacker.core.element.block.RPBlock;
+import com.marcusslover.resourcepacker.core.element.texture.RPTexture;
+import com.marcusslover.resourcepacker.core.registry.RPBlockRegistry;
+import com.marcusslover.resourcepacker.util.BlockUtil;
+import com.marcusslover.resourcepacker.util.FileUtil;
+
+import java.io.File;
+import java.util.List;
+
+public class BlockGenerator implements IGenerator<RPBlock, RPBlockRegistry> {
+    private final JsonObject log;
+
+    BlockGenerator() {
+        log = new JsonObject();
+    }
+
+    @Override
+    public JsonObject log() {
+        return log;
+    }
+
+    @Override
+    public void generate(File mc, File packer, RPBlockRegistry registry) {
+        List<RPBlock> list = registry.list();
+
+        File minecraftModels = FileUtil.get(mc, "models/item");
+        File blockStates = FileUtil.get(mc, "blockstates");
+        File blocks = FileUtil.get(packer, "textures/block");
+        File blockModels = FileUtil.get(packer, "models/block");
+        File itemModels = FileUtil.get(packer, "models/item");
+
+        File blockFile = FileUtil.safeFile(blockStates, "note_block.json");
+        JsonObject blockFileJson = new JsonObject();
+        JsonObject blockVariants = new JsonObject();
+
+        File itemFile = FileUtil.safeFile(minecraftModels, "note_block.json");
+        JsonObject itemFileJson = new JsonObject();
+        JsonArray itemVariants = new JsonArray(); // 'overrides'
+        itemFileJson.addProperty("parent", "minecraft:block/note_block");
+
+        int customModelData = 1;
+        int instrument = 0;
+        int note = 0;
+        boolean powered = false;
+        for (RPBlock block : list) {
+            if (note > BlockUtil.NOTE_LIMIT) {
+                note = 0;
+                instrument++;
+            }
+            /*Assign model data*/
+            block.data().setCustomModelData(customModelData);
+
+            RPTexture texture = block.texture();
+            String name = texture.name();
+            texture.copyFile(blocks); //Copy texture file.
+
+            /*Json block model*/
+            JsonObject blockModel = new JsonObject();
+            blockModel.addProperty("parent", "minecraft:block/cube_all");
+            JsonObject blockTextures = new JsonObject();
+            blockTextures.addProperty("all", "packer:block/" + name);
+            blockModel.add("textures", blockTextures);
+            File blockModelFile = FileUtil.safeFile(blockModels, name + ".json");
+            JsonUtil.writeFile(blockModelFile, blockModel); //Create file for model.
+
+            /*Block variant creation*/
+            JsonObject blockVariant = new JsonObject();
+            blockVariant.addProperty("model", "packer:block/" + name);
+
+            String blockPath = "instrument=" + BlockUtil.INSTRUMENTS[instrument] + ",note=" + note + ",powered=" + powered;
+            blockVariants.add(blockPath, blockVariant);
+
+            /*Json item model*/
+            JsonObject itemModel = new JsonObject();
+            JsonObject predicate = new JsonObject();
+            predicate.addProperty("custom_model_data", customModelData);
+            itemModel.add("predicate", predicate);
+            itemModel.addProperty("model", "packer:item/" + name);
+            itemVariants.add(itemModel);
+
+            /*Texture model*/
+            JsonObject textureModel = new JsonObject();
+            textureModel.addProperty("parent", "minecraft:block/cube_all");
+            JsonObject itemTexture = new JsonObject();
+            itemTexture.addProperty("all", "packer:block/" + name);
+            textureModel.add("textures", itemTexture);
+            File textureJson = FileUtil.safeFile(itemModels, name + ".json");
+            JsonUtil.writeFile(textureJson, textureModel);
+
+            if (powered) note++;
+            powered = !powered;
+            customModelData++;
+            /*Log creation*/
+            log.addProperty(name, blockPath);
+        }
+        /*Default variant*/
+        JsonObject itemModel = new JsonObject();
+        JsonObject predicate = new JsonObject();
+        predicate.addProperty("custom_model_data", customModelData);
+        itemModel.add("predicate", predicate);
+        itemModel.addProperty("model", "minecraft:item/note_block");
+        itemVariants.add(itemModel);
+
+        /*Block items*/
+        itemFileJson.add("overrides", itemVariants);
+        JsonUtil.writeFile(itemFile, itemFileJson);
+
+        /*Blocks*/
+        blockFileJson.add("variants", blockVariants);
+        JsonUtil.writeFile(blockFile, blockFileJson);
+    }
+}
---
+++ src/main/java/com/marcusslover/resourcepacker/util/FileUtil.java
@@ -0,0 +1,105 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 MarcusSlover
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package com.marcusslover.resourcepacker.util;
+
+import com.marcusslover.resourcepacker.api.IRegistry;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.FileTime;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FileUtil {
+    private FileUtil() {
+
+    }
+
+    public static File get(File parent, String path) {
+        if (path.contains("/")) {
+            String name = path.split("/")[0];
+            File file = safeDir(parent, name);
+            return get(file, path.replaceFirst(name + "/", ""));
+        }
+        return safeDir(parent, path);
+    }
+
+    public static File safeFile(File parent, String name) {
+        File child = new File(parent, name);
+        if (child.exists()) return child;
+        try {
+            boolean newFile = child.createNewFile();
+            if (newFile) return child;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void deleteFile(File file) {
+        if (!file.exists()) return;
+        @SuppressWarnings("unused")
+        boolean delete = file.delete();
+    }
+
+    public static File safeDir(File parent, String name) {
+        File child;
+        if (parent == null) child = new File(name);
+        else child = new File(parent, name);
+        if (child.exists()) return child;
+        boolean mkdirs = child.mkdirs();
+        if (mkdirs) return child;
+        return null;
+    }
+
+    public static void deleteDir(File file) {
+        File[] allContents = file.listFiles();
+        if (allContents != null) for (File f : allContents) deleteDir(f);
+        @SuppressWarnings("unused")
+        boolean delete = file.delete();
+    }
+
+    public static <T> List<T> sortByFileCreated(IRegistry<T> reg, Function<T, File> function) {
+        return reg.list()
+                .stream()
+                .sorted(Comparator.comparingLong(file -> getCreationTime(function.apply(file))))
+                .collect(Collectors.toList());
+    }
+
+
+    private static long getCreationTime(File file) {
+        try {
+            Object l = Files.getAttribute(file.toPath(), "creationTime");
+            return ((FileTime) l).toMillis();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+}
